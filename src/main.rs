b7f038118611969711f770abda4b652a77a096e2
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-01-23 16:50:11 +0100

    make move function working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::Error;
+use std::{fmt::Error, char};
 
 pub use crossterm_input::{input, AsyncReader, InputEvent, KeyEvent, MouseButton, MouseEvent, SyncReader, TerminalInput};
 pub use colorized::*;
@@ -6,30 +6,94 @@ pub use colorized::*;
 type Title = char;
 type Board = Vec<Vec<Title>>;
 
-enum UserAction {
-    MoveUp,
-    MoveDown,
-    MoveLeft,
-    MoveRight,
-    None,
-    Quit
-}
 
 fn get_user_input() -> char {
     let mut input = input();
-    match input {
+    match input.read_char() {
         Ok(c) => c,
         Err(e) => {
-            eprintln!("An error occurred: {}", e);
+            'e'
+        }
+    }   
+}
+fn move_up(coordinates: Vec<(u32, u32)>, board: &mut Vec<Vec<char>>) {
+    if let Some((x, y)) = coordinates.get(0) {
+        // Make sure we are not on the first row and the coordinates are within the board boundaries
+        if *y > 0 && *y as usize <= board.len() && *x as usize <= board[0].len() {
+            let x_usize = *x as usize;
+            let y_usize = *y as usize;
+
+            // Swap the character with the element above it
+            let temp = board[y_usize - 1][x_usize];
+            board[y_usize - 1][x_usize] = board[y_usize][x_usize];
+            board[y_usize][x_usize] = temp;
+        }
     }
-    
+}
+fn move_down(coordinates: Vec<(u32, u32)>, board: &mut Vec<Vec<char>>) {
+    if let Some((x, y)) = coordinates.get(0) {
+        if *y as usize + 1 < board.len() {
+            let x_usize = *x as usize;
+            let y_usize = *y as usize;
+
+            let temp = board[y_usize + 1][x_usize];
+            board[y_usize + 1][x_usize] = board[y_usize][x_usize];
+            board[y_usize][x_usize] = temp;
+        }
+    }
+}
+
+fn move_right(coordinates: Vec<(u32, u32)>, board: &mut Vec<Vec<char>>) {
+    if let Some((x, y)) = coordinates.get(0) {
+        if *x as usize + 1 < board[0].len() {
+            let x_usize = *x as usize;
+            let y_usize = *y as usize;
+
+            let temp = board[y_usize][x_usize + 1];
+            board[y_usize][x_usize + 1] = board[y_usize][x_usize];
+            board[y_usize][x_usize] = temp;
+        }
+    }
+}
+
+fn move_left(coordinates: Vec<(u32, u32)>, board: &mut Vec<Vec<char>>) {
+    if let Some((x, y)) = coordinates.get(0) {
+        if *x > 0 {
+            let x_usize = *x as usize;
+            let y_usize = *y as usize;
+
+            let temp = board[y_usize][x_usize - 1];
+            board[y_usize][x_usize - 1] = board[y_usize][x_usize];
+            board[y_usize][x_usize] = temp;
+        }
+    }
+}
+
+fn handle_input(usr_input: char, board: &mut Board) {
+    let coordinates: Vec<(u32, u32)> = find_player_in_board(board);
+    match usr_input {
+        'w' => move_up(coordinates ,board),
+        'a' => move_left(coordinates, board),
+        's' => move_down(coordinates, board),
+        'd' => move_right(coordinates, board),
+        _ => println!("Error")
+    }
 }
 fn init_board() -> Board {
     vec![vec!['#';16];16]
 }
-
-fn set_player_in_board(board: &mut Board){
-
+fn find_player_in_board(board: &mut Board) -> Vec<(u32, u32)> {
+    let mut coordinates = Vec::new();
+    for (y, row) in board.iter().enumerate() {
+        for (x, &char) in row.iter().enumerate() {
+            if char == '@' {
+                coordinates.push((x as u32, y as u32));
+            }
+        }
+    }
+    coordinates
+}
+fn set_player_in_board(board: &mut Board) {
     board[8][8] = '@';
 }
 fn game_over(input: char) -> bool {
@@ -62,6 +126,7 @@ fn main() {
     while  !game_over(usr_input) {
         print_board(&mut board);
         usr_input = get_user_input();
+        handle_input(usr_input, &mut board)
     }
     println!("Hello, world!");
 }
