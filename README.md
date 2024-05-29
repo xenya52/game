7c37977d91d2fba3c584a5005719c7a92643dc73
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-05-29 18:18:25 +0200

    Implement a Blocks and Blocks_type to implement a new Board

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
  
 - implement regrowable trees
 - Make a "overworld size class and extend the overwold after a specific amount of turns"
+- Make a functionary inventory
 - implement buildable structures
+- TODO find MAX u32 function for Water
 - Write a good md
 - Rework your game loop
--- src/world/world.rs
+++ src/world/world.rs
@@ -1,4 +1,4 @@
-use std::collections::btree_map::IterMut;
+use std::{u8::MAX, vec};
 
 use crate::game_logic::Materials;
 
@@ -7,23 +7,108 @@ use crate::game_logic::Materials;
 /////////
 pub type Board = Vec<Vec<char>>;
 pub type BBoard = Vec<Vec<Block>>;
+#[derive(Clone)]
 pub struct Block_Type {
     pub name: String,
     pub durability: u32, // 3 = three hits
     pub drop: Vec<Materials>
 }
+impl Block_Type {
+    pub fn new(_name: String, _durability: u32, _drop: Vec<Materials>) -> Self {
+        Block_Type {
+            name: _name,
+            durability: _durability,
+            drop: _drop
+        }
+    }
+    pub fn new_predefined_set() -> Vec<Block_Type> {
+        //[0] Stone, [1] Dirt, [2] Water, [3] Wood, [4] Food
+        let stone: Block_Type = Block_Type::new(
+            "Stone".to_string(), 4, 
+            vec![Materials::new(0, 1)]
+        );
+        let dirt: Block_Type = Block_Type::new(
+            "dirt".to_string(), 2,
+            vec![Materials::new(0, 0)]
+        );
+        let water: Block_Type = Block_Type::new(
+            "water".to_string(), 999, 
+            vec![Materials::new(0, 0)]
+        );
+        let wood: Block_Type = Block_Type::new(
+            "wood".to_string(), 2, 
+            vec![Materials::new(1, 0)]
+        );
+        let food: Block_Type = Block_Type::new(
+            "food".to_string(), 2, 
+            vec![Materials::new(0, 0)]
+        );
+        let air: Block_Type = Block_Type::new(
+            "air".to_string(), 0, 
+            vec![Materials::new(0, 0)]
+        );
+
+        return vec![stone, dirt, water, wood, food, air] 
+    }
+}
+#[derive(Clone)]
+
 pub struct Block {
     pub number: u32,
     pub display_ascii: char,
-    pub display_color: String
+    pub display_color: String,
+    pub block_type: Block_Type
 }
+// Returns a pack of blocks instead of a  specific 
 impl Block {
-    pub fn new(number: u32, _display_ascii: char, _display_color: String) -> Self {
+    pub fn new(_number: u32, _display_ascii: char, _display_color: String, _block_type: Block_Type) -> Self {
         Block {
-            number: number,
+            number: _number,
             display_ascii: _display_ascii,
-            display_color: _display_color
+            display_color: _display_color,
+            block_type: _block_type
         }
+    } 
+    pub fn new_predefined_set() -> Vec<Block> {
+        let temp = Block_Type:: new_predefined_set();
+        //[0] Stone, [1] Dirt, [2] Water, [3] Wood, [4] Food
+        let stone: Block = Block::new(
+            0, 
+            '#',
+            "gray".to_string(), 
+            temp[0].clone()
+        );
+        let dirt: Block = Block::new(
+            1,
+             '#', 
+             "brown".to_string(),
+            temp[1].clone()
+        );
+        let water: Block = Block::new(
+            2, 
+            '≈',
+            "blue".to_string(),
+            temp[2].clone()
+        );
+        let wood: Block = Block::new(
+            3, 
+            'H',
+            "brown".to_string(), 
+            temp[3].clone()
+        );
+        let food: Block = Block::new(
+            4,
+            'O',
+            "red".to_string(),
+            temp[4].clone()
+        );
+        let air: Block = Block::new(
+            5, 
+            ' ', 
+            "".to_string(),
+             temp[5].clone()
+        );
+        return vec![stone,dirt,water,wood,food, air];
     }
 }
 pub struct World {
