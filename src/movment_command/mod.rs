25cda8973667e6926c2844c576ac7e10aaff2fe2
Lucy Hühn <666nikkineko@gmail.com>
2024-08-20 08:31:20 +0200

    implementing the command pattern for movment - create a new folder, trying to follow the tutorial - The tutorial // https://refactoring.guru/design-patterns/command/rust/example

---
+++ src/movment_command/mod.rs
@@ -0,0 +1,14 @@
+mod up;
+mod down;
+mod left;
+mod right;
+
+pub use up::UpCommand;
+pub use down::DownCommand;
+pub use left::LeftCommand;
+pub use right::RightCommand;
+
+pub trait Command {
+    fn execute(&mut self, app: &mut cursive::Cursive) -> bool;
+    fn undo(&mut self, app: &mut cursive::Cursive);
+}
