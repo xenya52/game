69fbcde7253ebb3239b760d58364cec7efb72647
Lucy Hühn <666nikkineko@gmail.com>
2024-08-21 21:12:46 +0200

    working on movement function

--- src/movment_command/up.rs
+++ src/movment_command/up.rs
@@ -1,3 +1,23 @@
+
 pub struct UpCommand {
   backup: String,
+}
+// https://refactoring.guru/design-patterns/command/rust/example
+impl Command for UpCommand {
+  fn execute(&mut self, player: &mut Player) -> bool {
+      let mut movement = player.y;
+
+      app.with_user_data(|context: &mut AppContext| {
+          self.backup = editor.get_content().to_string();
+          context.clipboard = self.backup.clone();
+          editor.set_content("".to_string());
+      });
+
+      true
+  }
+
+  fn undo(&mut self, app: &mut Cursive) {
+      let mut editor = app.find_name::<EditView>("Editor").unwrap();
+      editor.set_content(&self.backup);
+  }
 }
